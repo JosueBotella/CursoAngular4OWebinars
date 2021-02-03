a8e05fe08163c559ffcf8d86627011e40d0e658a
JosueBotella <josuebotella@gmail.com>
2021-02-03 20:21:12 +0100

    1 file modified

--- src/app/talks/talks.component.ts
+++ src/app/talks/talks.component.ts
@@ -5,15 +5,19 @@ import { Component, OnInit } from "@angular/core";
   //templateUrl: './talks.component.html',
   template: `
     <input type="text" (keyup)="onKeyUp(search.value)" #search />
-    <p [ngStyle]="style" *ngFor="let talk of talks">{{ talk.title }}</p>
+    <p [ngStyle]="style" *ngFor="let talk of talks; let i = index">
+      <span *ngIf="display(i)">{{ i }}</span> {{ talk.title }}
+    </p>
   `,
   styleUrls: ["./talks.component.css"]
 })
 export class TalksComponent implements OnInit {
   talks: Array<any> = [
-    {title:"Angular rocks"},
-    {title: "Android 101"}
-    ]
+    { title: "Angular rocks" },
+    { title: "Android 101" },
+    { title: "Android 201" },
+    { title: "Android 301" }
+  ];
 
   style = {
     "background-color": "lightgray",
@@ -23,7 +27,9 @@ export class TalksComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {}
-
+  display(i) {
+    return i > 0;
+  }
   onKeyUp(value) {
     console.log("clicked!!" + value);
   }
