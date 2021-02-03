29561c3e3b8cdddf4e25009d4e1070a248d469d6
JosueBotella <josuebotella@gmail.com>
2021-02-03 20:04:34 +0100

    Directivas

--- src/app/talks/talks.component.ts
+++ src/app/talks/talks.component.ts
@@ -5,12 +5,16 @@ import { Component, OnInit } from "@angular/core";
   //templateUrl: './talks.component.html',
   template: `
     <input type="text" (keyup)="onKeyUp(search.value)" #search />
-    <p [ngStyle]="style">{{ title }}</p>
+    <p [ngStyle]="style" *ngFor="let talk of talks">{{ talk.title }}</p>
   `,
   styleUrls: ["./talks.component.css"]
 })
 export class TalksComponent implements OnInit {
-  title: string = "Angular Rocks";
+  talks: Array<any> = [
+    {title:"Angular rocks"},
+    {title: "Android 101"}
+    ]
+
   style = {
     "background-color": "lightgray",
     padding: "10px"
@@ -21,7 +25,6 @@ export class TalksComponent implements OnInit {
   ngOnInit() {}
 
   onKeyUp(value) {
-    this.title = "it's clicked";
     console.log("clicked!!" + value);
   }
 }
